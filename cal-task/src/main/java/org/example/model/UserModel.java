35f74e9f9c8e1c9873a4b1cdd7787fdf0740c892
ohyw <vvjiango@gmail.com>
2024-04-23 15:45:06 +0800

    add lombok project

---
+++ cal-task/src/main/java/org/example/model/UserModel.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserModel {
+    private String username;
+    private String password;
+}
