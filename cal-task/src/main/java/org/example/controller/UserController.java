cbba9701215667b79197b32423135ca83b9ca695
ohyw <vvjiango@gmail.com>
2024-04-25 12:41:18 +0800

    add result model

--- cal-task/src/main/java/org/example/controller/UserController.java
+++ cal-task/src/main/java/org/example/controller/UserController.java
@@ -1,9 +1,15 @@
 package org.example.controller;
 
+import org.example.model.ResultModel;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @GetMapping
+    public ResultModel list() {
+        return null;
+    }
 }
